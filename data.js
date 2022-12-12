55205f7c523829a7e87bd0d322bf11bfde2e68e0
Amy <86963506+aeposten@users.noreply.github.com>
2022-12-12 10:32:07 -0800

    Added base html, css, js files

---
+++ data.js
@@ -0,0 +1,23 @@
+export const versions = [
+    {
+        version: "Version One",
+        screenshot:"",
+        github: "https://github.com/aeposten/chore-chart",
+        deployed: "https://helmis-chore-chart.netlify.app/",
+        technologies: [`<i class="fa-brands fa-html5"></i>`, 
+                `<i class="fa-brands fa-css3-alt"></i>`, 
+                `<i class="fa-brands fa-square-js"></i>`],
+        description: ""
+    },
+    {
+        version: "Version Two",
+        screenshot:"",
+        github: "https://github.com/aeposten/chore-chart-2",
+        deployed: "https://helmis-chore-chart2.netlify.app/",
+        technologies: [`<i class="fa-brands fa-html5"></i>`, 
+                `<i class="fa-brands fa-css3-alt"></i>`, 
+                `<i class="fa-brands fa-square-js"></i>`,
+                `<i class="fa-solid fa-mobile-screen-button"></i>`],
+        description: ""
+    }
+]
---
+++ index.js
@@ -0,0 +1,1 @@
+import { versions } from "./data";
