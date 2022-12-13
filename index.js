77d4666838b97cd3253eb6c29354731d349d4e1d
Amy <86963506+aeposten@users.noreply.github.com>
2022-12-12 17:04:34 -0800

    Refactored render function

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const hero = {
-  elementId: "magical-toddler",
-  name: "Helmi",
-  avatar: "img/magical-helmi.png",
-  health: 60,
-  diceCount: 3,
-};
+const characterData = {
+  hero: {
+    elementId: "magical-toddler",
+    name: "Helmi",
+    avatar: "img/magical-helmi.png",
+    health: 60,
+    diceCount: 3,
+  },
 
-const enemy = {
-  elementId: "enemy",
-  name: "Pizza Boxer",
-  avatar: "img/pizza-boxer.png",
-  health: 10,
-  diceCount: 1,
+  enemy: {
+    elementId: "enemy",
+    name: "Pizza Boxer",
+    avatar: "img/pizza-boxer.png",
+    health: 10,
+    diceCount: 1,
+  },
 };
 
 function rollD6() {
@@ -21,6 +23,7 @@ function rollD6() {
 function renderDiceRolls(diceCount) {
   return new Array(diceCount).fill().map((roll) => rollD6());
 }
+
 function renderDiceHTML(diceCount) {
   return renderDiceRolls(diceCount)
     .map(
@@ -36,22 +39,29 @@ function Character(data) {
   this.renderCharacterHtml = () => {
     const { elementId, name, avatar, health, diceCount } = data;
 
-    document.getElementById(elementId).innerHTML = `<div class="character-card">
-                    <h2 class="name"> ${name} </h2>
-                    <div class="img-container">
-                      <img class="avatar"  src="${avatar}" />
-                      <img class="milk" src="./img/milk.png" />
-                    </div>
-                    <div class="health">health: <b> ${health} </b></div>
-                    <div class="heart-container">
-                        <div class="hearts"> ${renderDiceHTML(diceCount)} </div>
-                    </div>
-                </div>`;
+    return `<div class="character-card">
+                <h2 class="name"> ${name} </h2>
+                <div class="img-container">
+                    <img class="avatar"  src="${avatar}" />
+                    <img class="milk" src="./img/milk.png" />
+                </div>
+                <div class="health">health: <b> ${health} </b></div>
+                <div class="heart-container">
+                    <div class="hearts"> ${renderDiceHTML(diceCount)} </div>
+                </div>
+            </div>`;
   };
 }
 
-const helmi = new Character(hero);
-helmi.renderCharacterHtml();
+function render() {
+  document.getElementById(helmi.elementId).innerHTML =
+    helmi.renderCharacterHtml();
+    
+  document.getElementById(pizzaBoxer.elementId).innerHTML =
+    pizzaBoxer.renderCharacterHtml();
+}
 
-const pizzaBoxer = new Character(enemy);
-pizzaBoxer.renderCharacterHtml();
+const helmi = new Character(characterData.hero);
+const pizzaBoxer = new Character(characterData.enemy);
+
+render();
